67b70363753208ddf5ee4b41e36f644198b9a8da
Eric Todd <39493826+EricMTodd@users.noreply.github.com>
2022-10-25 16:57:27 -0600

    Minor styling and formatting changes.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,8 @@
 const Home = () => {
   return(
     <div id='home'>
-      <h1 style={{margin: '0 auto'}}>Eric M. Todd's Reactified Submission for Ksense</h1>
-      <p>This is the React version of the challenge! Click around on the names at the top and watch the magic happen. It took a little longer than I had expected because I was trying to do everything by filtering everything from the top level component in order to only make one API call. Unfortunately I couldn't make that work, so I just stuck with the good ol' make it work first philosophy.</p>
+      <h1 style={{margin: '0 auto', textAlign: 'center'}}>Eric M. Todd's Reactified Submission for Ksense</h1>
+      <p style={{fontSize: '24px'}}>This is the React version of the challenge! Click around on the names at the top and watch the magic happen. It took a little longer than I had expected because I was trying to do everything by filtering everything from the top level component in order to only make one API call. Unfortunately I couldn't make that work, so I just stuck with the good ol' make it work first philosophy.</p>
     </div>
   )
 }
